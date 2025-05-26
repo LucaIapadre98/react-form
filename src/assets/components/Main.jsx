4ea88439fb59af2a03d9e7b247a52c0b2bf02e03
LucaIapadre98 <luca.iap.1998@gmail.com>
2025-05-26 17:13:23 +0200

    Modifiche finali

--- src/assets/components/Main.jsx
+++ src/assets/components/Main.jsx
@@ -3,25 +3,27 @@ import { useState } from "react";
 export default function Main(){
     // GESTIONE DELL'ARRAY //
     const [listTitle, setListTitle] = useState(["Html","Css","JavaScript","Express"]);
-    //GESTIONE DEL UOVO INPUT //
+
+    //GESTIONE DEL NUOVO INPUT //
     const [newTitle, setNewTitle] = useState("");
 
-    // PREVIENE L'INVIO DEL FORM IN PAGINA //
+    // FUNZIONE CHE PREVIENE L'INVIO DEL FORM IN PAGINA //
     const handleFormSubmit =(e) => {
         e.preventDefault(); 
         const newListTitle = [...listTitle, newTitle];
         setListTitle(newListTitle);
     };
     // FUNZIONE PER L'ELIMINAZIONE DI UN INPUT IN PAGINA //
-    const handleDeleteTitle = (indexToDelete) =>{
-        const newListTitle = listTitle.filter((title,index) => index != indexToDelete);
+    const handleDeleteTitle = (indexDelete) =>{
+        const newListTitle = listTitle.filter((index) => index != indexDelete);
+        setListTitle(newListTitle);
     };
     return (
         <main>
             <div className="container">
                 <ul>
                    {listTitle.map((title, index) =>(
-                    <li key={index}>{title}</li>
+                    <li key={index} onClick={() =>handleDeleteTitle(index)}>{title}</li>
                     ))}
                 </ul>
                 <form onSubmit={handleFormSubmit}>
@@ -31,8 +33,8 @@ export default function Main(){
                         className="form-control" 
                         type="text"
                     />
-                    <button className="btn btn-primary">Aggiungi</button>
-                    <button className="btn btn-danger" onClick={()=>handleDeleteTitle()}>Elimina</button>
+                    <button id="btn-add" className="btn btn-primary">Aggiungi</button>
+                    <button id="btn-remove" className="btn btn-danger">Elimina</button>
                 </form>
             </div>
         </main>
